9cb3882ec0e2442fd81911fe156383d5c1526ff1
Brian Grant and Kyle Helyar <kyle@>
2016-08-02 10:05:30 -0700

    add time using moment

--- js/time-interface.js
+++ js/time-interface.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
   $('#time').text(moment());
+  $('.time-zone').submit(function(event){
+    event.preventDefault();
+    $('#time').text(moment.tz($('.time-zone').val()));
+  });
+
 });
